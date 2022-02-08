6c82aace1bf322c573679154b936e9ab405d1941
naser23 <naserabdulkadir0@gmail.com>
2022-02-07 20:25:07 -0500

    finished the styling for the HeroImg component for all devices. will work on the right side tomorrow.

---
+++ sign-up-form/src/components/RightSide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RightSide() {
+  return <section className="right-side">
+      <div className="top-with-header">
+        
+      </div>
+  </section>;
+}
+
+export default RightSide;
